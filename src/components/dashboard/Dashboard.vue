9f16832962983c393bb90674d9e3bc3a1e929169
Eliezer Pacheco Jiménez <elix-13@hotmail.com>
2020-03-09 03:20:29 -0700

    Fixed variable names in dashboard

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -7,23 +7,33 @@
             <p class="display-1 text--primary">
               Heroes
             </p>
-            <p>Available</p>
-            <div class="text--primary">
-              {{heroesSummary.available}}
+            <div v-if="heroesSummary != null">
+              <p>Available</p>
+              <div class="text--primary">
+                {{heroesSummary.available}}
+              </div>
+              <p>Most Popular Race</p>
+              <div class="text--primary">
+                {{heroesSummary.mostPopularRace.name}}
+              </div>
+              <p>Most Popular Class</p>
+              <div class="text--primary">
+                {{heroesSummary.mostPopularClass.name}}
+              </div>
+              <p>Most Popular Weapon</p>
+              <div class="text--primary">
+                {{heroesSummary.mostPopularWeapon.name}}
+              </div>
             </div>
-            <p>Most Popular Race</p>
-            <div class="text--primary">
-              {{heroesSummary.mostPopularRace.name}}
-            </div>
-            <p>Most Popular Class</p>
-            <div class="text--primary">
-              {{heroesSummary.mostPopularClass.name}}
-            </div>
-            <p>Most Popular Weapon</p>
-            <div class="text--primary">
-              {{heroesSummary.mostPopularWeapon.name}}
+            <div v-else>
+              <div class="text--primary">
+                Heroes summary not available
+              </div>
             </div>
           </v-card-text>
+          <v-card-actions>
+            
+          </v-card-actions>
         </v-card>
       </v-col>
 
@@ -33,19 +43,29 @@
             <p class="display-1 text--primary">
               Monsters
             </p>
-            <p>Available</p>
-            <div class="text--primary">
-              {{monstersSummary.available}}
+            <div v-if="monstersSummary != null">
+              <p>Available</p>
+              <div class="text--primary">
+                {{monstersSummary.available}}
+              </div>
+              <p>Most Popular Race</p>
+              <div class="text--primary">
+                {{monstersSummary.mostPopularRace.name}}
+              </div>
+              <p>Most Popular Ability</p>
+              <div class="text--primary">
+                {{monstersSummary.mostPopularAbility.name}}
+              </div>
             </div>
-            <p>Most Popular Race</p>
-            <div class="text--primary">
-              {{monstersSummary.mostPopularRace.name}}
-            </div>
-            <p>Most Popular Ability</p>
-            <div class="text--primary">
-              {{monstersSummary.mostPopularAbility.name}}
+            <div v-else>
+              <div class="text--primary">
+                Monsters summary not available
+              </div>
             </div>
           </v-card-text>
+          <v-card-actions>
+            
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
@@ -61,21 +81,19 @@
       this.fetchMonstersSummary();
     },
     data: () => ({
-      heroesSummary: {},
-      monstersSumary: {}
+      heroesSummary: null,
+      monstersSummary: null
     }),
     methods: {
       fetchHeroesSummary: function(){
         axios.get('http://localhost:8000/api/hero/dashboard')
           .then((response) => {
-            console.log(response);
             this.heroesSummary = response.data;
           });
       },
       fetchMonstersSummary: function(){
         axios.get('http://localhost:8000/api/monster/dashboard')
           .then((response) => {
-            console.log(response);
             this.monstersSummary = response.data;
           });
       }
